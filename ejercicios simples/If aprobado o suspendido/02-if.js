bccd49c82d28d431ab4432e359442ac5d8f49786
DanielCamelCase <42586858+DanielCamelCase@users.noreply.github.com>
2018-08-23 18:55:07 +0200

    Add files via upload

---
+++ ejercicios simples/If aprobado o suspendido/02-if.js
@@ -0,0 +1,46 @@
+'mode strict'
+//con if - saber si alguien aprueba o suspende simplemente 
+// con 5 aprueba 
+
+var persona = 7
+var persona2 = 2
+
+if ( persona >= 5 ) {
+
+document.write ( persona +' has aprobado' + "</br>")
+console.log(persona + ' has aprobado')
+}
+
+else {
+
+    document.write ( persona +' has suspendido'+ "</br>")
+    console.log(persona + ' has suspendido' )
+}
+
+if ( persona2 >= 5 ) {
+
+    document.write ( persona2 +' has aprobado' + "</br>")
+    console.log(persona2 + ' has aprobado' )
+    }
+    
+    else {
+    
+        document.write ( persona2 +' has suspendido' + "</br>")
+        console.log(persona2 + ' has suspendido' )
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+// ejercicio para saber las notas en base a su puntuacion (suspenso menos de 5, 5 suficiente ,
+// 6 bien, 7 notable bajo , 8 notable alto ,9 sobresaliente bajo y 10 sobresaliente)
+// una persona por cada nota desde 4 hasta 10 
+
---
+++ ejercicios simples/convertir entre farenheit y celsius/01-variables.js
@@ -0,0 +1,11 @@
+'mode strict'
+
+var f = 90
+var c = 0 
+
+c = ( f - 32 ) * ( 5 / 9 )
+
+document.write( f +' ' + 'grados fareheit es igual a ' + c +' ' + 'grados centigrados')
+console.log( f +' ' + 'grados fareheit es igual a ' + c +' ' + 'grados centigrados' )
+console.log(c)
+
